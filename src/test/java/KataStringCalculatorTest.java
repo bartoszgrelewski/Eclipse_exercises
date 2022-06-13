186a23a21e35858ca8a9bb9d8558bd95e60aa330
Bartek Grelewski <stilus43@gmail.com>
2022-06-13 22:37:38 +0200

    Step 2
    Allow the Add method to handle an unknown amount of numbers.

--- src/test/java/KataStringCalculatorTest.java
+++ src/test/java/KataStringCalculatorTest.java
@@ -11,4 +11,9 @@ public class KataStringCalculatorTest {
         void resultNumberForOne(){
         Assertions.assertThat(StringCalculator.add("15")).isEqualTo(15);
         }
+    @Test
+        void resultSumNumbers(){
+        Assertions.assertThat(StringCalculator.add("1,2,3,4,5")).isEqualTo(15);
+    }
+
 }
